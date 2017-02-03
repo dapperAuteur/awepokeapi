9ff1c88a30e662916b144b15c7b80b6309c3bce2
awe <aweful@awews.com>
2017-02-03 14:37:00 -0700

    start making loop to construct pokemon object

--- js/questions1.js
+++ js/questions1.js
@@ -1,6 +1,7 @@
 /**
  *
  */
+/*jshint esversion: 6 */
 function strict() {
   // Function-level strict mode syntax
   'use strict';
@@ -12,6 +13,8 @@ function strict() {
 			let inputTitle = input.charAt(0).toUpperCase() + input.substring(1);
 			let player1 = "player_1";
 			let playerScore = 100;
+			let randomPoke;
+			let pokeTraits = ["weight", "height", "species.name", "base_experience"];
 			console.log(input + " " + player1 + "\'s score is: " + playerScore + " " + inputTitle);
 
 			//create cookie to store user score
@@ -20,12 +23,19 @@ function strict() {
 			
 
 			//create an array of random pokemon to test user's knowledge
-			let arr = [];
+			let arrDummyPoke = [];
 			for(let i = 0; i < 4; i++) {
-				let randomPoke = Math.floor( Math.random() * 721 );
-				arr[i] = randomPoke;
+				randomPoke = Math.floor( Math.random() * 721 );
+				arrDummyPoke[i] = [randomPoke];
+				console.log(arrDummyPoke[i]);
+				console.log(pokeTraits[i]);
+				for(let j = 0; j < pokeTraits.length; j++) {
+					console.log(arrDummyPoke[i]);
+					console.log(pokeTraits[j]);
+					arrDummyPoke[i][j+1] = pokeTraits[j];
+				}
 			}
-			console.log(arr);
+			console.log(arrDummyPoke);
 
 			//grab data from api about user's chosen pokemon
 			$.ajax({
@@ -39,18 +49,30 @@ function strict() {
 
 				}
 			});
-
+			
 			//refactor to a for loop
-			for(let i = 0; i < arr.length; i++) {
+			for(let i = 0; i < arrDummyPoke.length; i++) {
 				$.ajax({
 					method:"GET",
-					url: "//pokeapi.co/api/v2/pokemon/" + arr[i] + "/",
+					url: "//pokeapi.co/api/v2/pokemon/" + arrDummyPoke[i] + "/",
 					success: function(data) {
 						console.log(data);
 						$("#pokemon_" + i).html(data.name);
 					}
 				});
 			}
+//
+//			//refactor to a for loop
+//			for(let i = 0; i < arrDummyPoke.length; i++) {
+//				$.ajax({
+//					method:"GET",
+//					url: "//pokeapi.co/api/v2/pokemon/" + arrDummyPoke[i] + "/",
+//					success: function(data) {
+//						console.log(data);
+//						$("#pokemon_" + i).html(data.name);
+//					}
+//				});
+//			}
 
 		});
 		//print cookie data to console
