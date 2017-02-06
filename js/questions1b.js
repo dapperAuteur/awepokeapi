d83e6f5997e79c46cae73e960aae28f2edf357b4
awe <aweful@awews.com>
2017-02-06 14:44:59 -0700

    create a button to appear after answering question, not working

--- js/questions1b.js
+++ js/questions1b.js
@@ -7,36 +7,54 @@ function strict() {
   'use strict';
 	$(document).ready(function() {
 	// DOM is ready
-		let player1 = "player_1";
+		$(".nextQuestion").hide();
+		let player1 = "Player";
 		let playerScore = 100;
 		//user enter's pokemon and game starts
 		$("#start_button").click(function() {
 			let input =  $("#userInput").val().toLocaleLowerCase();
 			let inputTitle = input.charAt(0).toUpperCase() + input.substring(1);
 			let randomPoke;
-			let arrPokeWeight = [];
-			let arrPokeHeight = [];
-			let arrPokeSpecies = [];
-			let arrPokeBaseExp = [];
 			let arrDummyPokeNum = [];
 			let arrDummyPokeData = [];
+			let objPlayerPoke = {};
 			let objPoke0 = {};
 			let objPoke1 = {};
 			let objPoke2 = {};
 			let objPoke3 = {};
 //			let pokeURL = "https://pokeapi.co/api/v2/pokemon/";
-			let pokeTraits = function(name, weight, height, speciesName, baseExperience) {
+			let pokeTraits = function(name, moves, height, speciesName, baseExperience) {
 				this.name = name;
-				this.weight = weight;
+				this.moves = moves;
 				this.height = height;
 				this.speciesName = speciesName;
 				this.baseExperience = baseExperience;
 			};
+			
+			var showNext = function() {
+				$(".nextQuestion").show();
+			};
+			//question2
+			let question2 = function(objplr, obj0, obj1, obj2, obj3, inCorrect, correct) {
+				$("#pokemon_0").html(objPoke0.height);
+				$("#pokemon_1").html(objPoke1.height);
+				$("#pokemon_2").html(objPoke2.height);
+				$("#pokemon_3").html(objPoke3.height);
+				$("#pokemon_4").html(objPlayerPoke.height);
+				document.getElementById("pokemon_0").addEventListener("click", inCorrect);
+				document.getElementById("pokemon_1").addEventListener("click", inCorrect);
+				document.getElementById("pokemon_2").addEventListener("click", inCorrect);
+				document.getElementById("pokemon_3").addEventListener("click", inCorrect);
+				document.getElementById("pokemon_4").addEventListener("click", correct);
+		};
+			
 			var inCorrect = function() {
 				playerScore -= 50;
 				console.log("incorrect works");
 				$("#player_score").html(playerScore);
-				alert("You Don't Know " + input);
+				$("#questionResult").html(player1 + " doesn't know " + inputTitle + "!");
+				question2();
+				$(".nextQuestion").show();
 
 			};
 		
@@ -44,8 +62,11 @@ function strict() {
 				playerScore += 100;
 				console.log("correct works");
 				$("#player_score").html(playerScore);
-				alert("You Do Know " + input);
+				$("#questionResult").html(player1 + " knows " + inputTitle + "!");
+				question2();
+				$(".nextQuestion").show();
 			};
+			
 			//create cookie to store user score
 			//$.cookie("player1", 0);
 			
@@ -70,39 +91,24 @@ function strict() {
 					console.log(arrDummyPokeNum[i]);
 					console.log("arrDummyPokeData[i]");
 					console.log(arrDummyPokeData[i]);
-					$("#pokemon_" + i).html(arrDummyPokeData[i].weight);
+					$("#pokemon_" + i).html(arrDummyPokeData[i].moves.length);
 					console.log("arrPoke[i]");
-					console.log(arrDummyPokeData[i].weight);
+					console.log(arrDummyPokeData[i].moves.length);
 					if(i === 0) {
-						objPoke0 = pokeTraits(data.name, data.weight, data.height, data.species, data.baseExperience);
+						objPoke0 = pokeTraits(data.name, data.moves.length, data.height, data.species, data.baseExperience);
 					} else if (i === 1) {
-						objPoke1 = pokeTraits(data.name, data.weight, data.height, data.species, data.baseExperience);
+						objPoke1 = pokeTraits(data.name, data.moves.length, data.height, data.species, data.baseExperience);
 					} else if (i === 2) {
-						objPoke2 = pokeTraits(data.name, data.weight, data.height, data.species, data.baseExperience);
+						objPoke2 = pokeTraits(data.name, data.moves.length, data.height, data.species, data.baseExperience);
 					} else {
-						objPoke3 = pokeTraits(data.name, data.weight, data.height, data.species, data.baseExperience);
+						objPoke3 = pokeTraits(data.name, data.moves.length, data.height, data.species, data.baseExperience);
 					}
-						
-					console.log(data.weight);
-					arrPokeWeight = data.weight;
-					console.log(arrPokeWeight[i]);
-					arrPokeHeight = data.height;
-					arrPokeSpecies = data.species;
-					arrPokeBaseExp = data.baseExperience;
-					//$("#pokemon_" + i).html(data.weight);
 					
+//					return objPoke0, objPoke1, objPoke2, objPoke3;
 				}
 			});
 				
 			}
-			
-			
-			
-//			for(let i = 0; i < arrPoke.length; i++) {
-//				$("#pokemon_" + i).html(arrPoke[i]);
-//				$("#pokemon_" + i).html(i);
-//				console.log(arrPoke[i]);
-//			}
 			
 
 			//grab data from api about user's chosen pokemon
@@ -115,29 +121,50 @@ function strict() {
 					$("#pokemon_chosen").html(inputTitle);
 					$("#pokemon_choices").html(inputTitle);
 					$("#player_score").html(playerScore);
-					$("#pokemon_4").html(data.weight);
+					$("#pokemon_4").html(data.moves.length);
+					$("#trait").html("moves");
+					objPlayerPoke = pokeTraits(data.name, data.moves.length, data.height, data.species, data.baseExperience);
+					return objPlayerPoke;
 
 				}
 			});
 			
-			$("#pokemon_0").html(objPoke0.weight);
-			$("#pokemon_1").html(objPoke1.weight);
-			$("#pokemon_2").html(objPoke2.weight);
-			$("#pokemon_3").html(objPoke3.weight);
+//			let isCorrect = function() {
+//				objPlayerPoke == 
+//			}
 			
-			document.getElementById("pokemon_0").addEventListener("click", inCorrect);
-			document.getElementById("pokemon_1").addEventListener("click", inCorrect);
-			document.getElementById("pokemon_2").addEventListener("click", inCorrect);
-			document.getElementById("pokemon_3").addEventListener("click", inCorrect);
-			document.getElementById("pokemon_4").addEventListener("click", correct);
+			$("#pokemon_0").html(objPoke0.moves.length);
+			$("#pokemon_1").html(objPoke1.moves.length);
+			$("#pokemon_2").html(objPoke2.moves.length);
+			$("#pokemon_3").html(objPoke3.moves.length);
 			
+			document.getElementById("pokemon_0").addEventListener("click", function() {
+				inCorrect();
+				showNext();
+																					  } );
+			document.getElementById("pokemon_1").addEventListener("click", function() {
+				inCorrect();
+				showNext();
+																					  } );
+			document.getElementById("pokemon_2").addEventListener("click", function() {
+				inCorrect();
+				showNext();
+																					  } );
+			document.getElementById("pokemon_3").addEventListener("click", function() {
+				inCorrect();
+				showNext();
+																					  } );
+			document.getElementById("pokemon_4").addEventListener("click", function() {
+				correct();
+				showNext();
+																					  } );
 			
-
+			question2(objPlayerPoke, objPoke0, objPoke1, objPoke2, objPoke3, inCorrect, correct);
+			
 
 		});
 		//print cookie data to console
 		//console.log($.cookie("player1"));
-		
 		
 	});
 }
